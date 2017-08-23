ff2bcd86bbf37de7d743a9e4883c4394d343e2ba
zonoise@gmail.com <zonoise@gmail.com>
2017-08-24 02:05:24 +0900

    use google-http-client

--- src/main/java/com/example/controller/BacklogAuthController.java
+++ src/main/java/com/example/controller/BacklogAuthController.java
@@ -1,6 +1,16 @@
 package com.example.controller;
 
 import com.example.forms.GetOauthAccessTokenForm;
+import com.google.api.client.http.*;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.util.Key;
+
+
+import com.google.api.client.util.GenericData;
 import com.nulabinc.backlog4j.auth.AccessToken;
 import com.nulabinc.backlog4j.auth.BacklogOAuthSupport;
 import com.nulabinc.backlog4j.conf.BacklogConfigure;
@@ -8,11 +18,13 @@ import com.nulabinc.backlog4j.conf.BacklogJpConfigure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 /**
@@ -53,7 +65,7 @@ public class BacklogAuthController {
     }
 
     @RequestMapping("/callback")
-    public String callback(@RequestParam("code") String code, Model model){
+    public String callback(@RequestParam("code") String code, Model model) throws IOException {
         try{
             BacklogConfigure configure = new BacklogJpConfigure(spaceId).apiKey(apiKey);
 
@@ -71,6 +83,16 @@ public class BacklogAuthController {
             model.addAttribute("rest_url",configure.getRestBaseURL()+"/oauth2/token");
             model.addAttribute("formData",f);
 
+            GenericData data = new GenericData();
+            data.put("grant_type","authorization_code");
+            data.put("code",code);
+            data.put("client_id",apiKey);
+            data.put("client_secret",apiSecret);
+
+            OAuthAccessTokenResponse d = getOAuthAccessToken(configure.getOAuthAccessTokenURL(),data);
+            if(null != d){
+                logger.info("token:{}",d.toString());
+            }
         }catch (MalformedURLException e){
             //todo
         }
@@ -78,6 +100,32 @@ public class BacklogAuthController {
         return "callback";
     }
 
+    private OAuthAccessTokenResponse getOAuthAccessToken(String url,GenericData data) throws IOException {
+        HttpTransport httpTransport = new NetHttpTransport();
+        try {
+            HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+            GenericUrl genericUrl = new GenericUrl(url);
+            HttpContent httpContent =  new UrlEncodedContent(data);
+            HttpRequest request = requestFactory.buildPostRequest(genericUrl,httpContent);
+            request.setLoggingEnabled(true);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType("application/x-www-form-urlencoded");
+            request.setHeaders(headers);
+            HttpResponse response = request.execute();
+            System.out.print(response.getStatusCode());
+            if(response.isSuccessStatusCode()){
+                String responseString = response.parseAsString();
+                logger.info("response:{}",responseString);
+                OAuthAccessTokenResponse d = response.parseAs(OAuthAccessTokenResponse.class);
+                return d;
+            }
+        }catch (java.io.IOException e){
+
+        } finally {
+            httpTransport.shutdown();
+        }
+        return null;
+    }
 
     @RequestMapping("/gettoken")
     public String getToken(@RequestParam("code") String code, Model model){
@@ -97,3 +145,13 @@ public class BacklogAuthController {
     }
 }
 
+class OAuthAccessTokenResponse extends GenericData {
+    @Key("token_type")
+    public String tokenType;
+    @Key("access_token")
+    public String accessToken;
+    @Key("expires_in")
+    public Integer expiresIn;
+    @Key("refresh_token")
+    public String refreshToken;
+}
